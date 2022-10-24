d93a762402b6c925cf28b9fbd9572b9f1572371c
Mariya Zakharova <zakharovamaria73@yandex.ru>
2022-10-24 15:49:53 +0400

    fix: next

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).send({
     message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
   });
+  next();
 });
 
 app.listen(PORT);
