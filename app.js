8fe5995ed180e9895264e7fbc401342138b055f7
Mariya Zakharova <zakharovamaria73@yandex.ru>
2022-11-05 23:42:51 +0400

    feat: env

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 const cors = require('cors');
 const { errors } = require('celebrate');
 const usersRouter = require('./routes/usersRouter');
